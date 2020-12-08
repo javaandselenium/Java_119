7b002118d4996f1cb2fbb91bda2eb1afcae81132
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-08 07:28:41 +0530

    Tokens

---
+++ src/basics/Test0.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		System.out.print(true);
+		System.out.print(false);
+		System.out.print("Hello java");
+		System.out.print('J');
+		System.out.print(1234);
+		System.out.print(34.56);
+
+	}
+
+}
---
+++ src/basics/Test1.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println('A');
+		System.out.println(1234);
+		System.out.println(89.00);
+		System.out.println(true);
+		System.out.println(false);
+
+	}
+
+}
