7d2667503797109df086246b0ac61bf1325afa7b
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-17 07:27:42 +0530

    Loops

---
+++ src/loops/Test4.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+	int i=1;
+	while(i<5) {
+		System.out.println("true");
+		break;
+	}
+	
+	System.out.println("hello");
+
+	}
+
+}
---
+++ src/loops/Test5.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		String sub="JAVA";
+		
+		while(sub=="JAVA") {
+			System.out.println("crrt sub");
+			break;
+		}
+		
+
+	}
+
+}
